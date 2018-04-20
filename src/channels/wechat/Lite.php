9480c585338e02f8246d4d38af4d20667d654fc9
xutongle <xutongle@gmail.com>
2018-04-20 15:25:45 +0800

    小程序支付

--- src/channels/wechat/Lite.php
+++ src/channels/wechat/Lite.php
@@ -19,14 +19,49 @@ use yuncms\transaction\models\TransactionCharge;
  */
 class Lite extends Wechat
 {
-
     /**
      * 发起支付
      * @param TransactionCharge $charge
      * @return TransactionCharge
+     * @throws Exception
+     * @throws \yii\base\Exception
+     * @throws \yii\base\InvalidConfigException
      */
     public function charge(TransactionCharge $charge)
     {
-        // TODO: Implement charge() method.
+        $params = [
+            'body' => $charge->body,
+            'out_trade_no' => $charge->outTradeNo,
+            'total_fee' => $charge->amount,
+            'fee_type' => $charge->currency,
+            'trade_type' => 'JSAPI',
+            'notify_url' => $this->getNoticeUrl(),
+            'spbill_create_ip' => Yii::$app->request->isConsoleRequest ? '127.0.0.1' : Yii::$app->request->userIP,
+            'device_info' => 'WEB',
+            'attach' => $charge->extra,
+        ];
+        if (isset($charge->metadata['openid'])) {
+            $params['openid'] = $charge->metadata['openid'];
+        } else if (isset($trade->user->socialAccounts['wechat'])) {
+            $weParams = $charge->user->socialAccounts['wechat']->getDecodedData();
+            $params['openid'] = $weParams['openid'];
+        } else {
+            throw new Exception ('Non-WeChat authorized login.');
+        }
+        $response = $this->sendRequest('POST', 'pay/unifiedorder',$params);
+        if ($response['return_code'] == 'SUCCESS') {
+            $tradeParams = [
+                'appId' => $this->appId,
+                'timeStamp' => time(),
+                'nonceStr' => $this->generateRandomString(32),
+                'package' => 'prepay_id=' . $response['prepay_id'],
+                'signType' => 'MD5',
+            ];
+            $tradeParams['paySign'] = $this->generateSignature($tradeParams);
+            $charge->setTransactionCredential($response['prepay_id'], $tradeParams);
+            return $charge;
+        } else {
+            throw new Exception($response['return_msg']);
+        }
     }
 }
