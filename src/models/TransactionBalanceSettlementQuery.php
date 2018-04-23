f765c802d61dc521bd30f015942c39f844eb211c
xutongle <xutongle@gmail.com>
2018-04-23 11:26:15 +0800

    余额结算

--- src/models/TransactionBalanceSettlementQuery.php
+++ src/models/TransactionBalanceSettlementQuery.php
@@ -9,15 +9,32 @@ namespace yuncms\transaction\models;
  */
 class TransactionBalanceSettlementQuery extends \yii\db\ActiveQuery
 {
-    /*public function active()
+    /**
+     * 获取已经结算
+     * @return $this
+     */
+    public function succeeded()
     {
-        return $this->andWhere('[[status]]=1');
-    }*/
+        return $this->andWhere(['status' => TransactionBalanceSettlement::STATUS_SUCCEEDED]);
+    }
 
-    /*public function active()
+    /**
+     * 获取已入账
+     * @return $this
+     */
+    public function credited()
     {
-        return $this->andWhere(['status' => TransactionBalanceSettlement::STATUS_PUBLISHED]);
-    }*/
+        return $this->andWhere(['status' => TransactionBalanceSettlement::STATUS_CREDITED]);
+    }
+
+    /**
+     * 获取未结算
+     * @return $this
+     */
+    public function created()
+    {
+        return $this->andWhere(['status' => TransactionBalanceSettlement::STATUS_CREATED]);
+    }
 
     /**
      * @inheritdoc
@@ -35,61 +52,5 @@ class TransactionBalanceSettlementQuery extends \yii\db\ActiveQuery
     public function one($db = null)
     {
         return parent::one($db);
-    }
-
-    /**
-     * 热门模型
-     * @param string $reference 计算字段
-     * @param float $pull 热度衰减下拉指数默认是1.8
-     * @return mixed
-     */
-    public function hottest($reference = 'views', $pull = 1.8)
-    {
-        return $this->orderBy(['(' . $reference . ' / pow((((UNIX_TIMESTAMP(NOW()) - created_at) / 3600) + 2),' . $pull . ') )' => SORT_DESC]);
-    }
-
-    /**
-     * 查询今日新增
-     * @return $this
-     */
-    public function dayCreate()
-    {
-        return $this->andWhere('date(created_at)=date(NOW())');
-    }
-
-    /**
-     * 查询本周新增
-     * @return $this
-     */
-    public function weekCreate()
-    {
-        return $this->andWhere('month(FROM_UNIXTIME(created_at)) = month(curdate()) AND week(FROM_UNIXTIME(created_at)) = week(curdate())');
-    }
-
-    /**
-     * 查询本月新增
-     * @return $this
-     */
-    public function monthCreate()
-    {
-        return $this->andWhere('month(FROM_UNIXTIME(created_at)) = month(curdate()) AND year(FROM_UNIXTIME(created_at)) = year(curdate())');
-    }
-
-    /**
-     * 查询本年新增
-     * @return $this
-     */
-    public function yearCreate()
-    {
-        return $this->andWhere('year(FROM_UNIXTIME(created_at)) = year(curdate())');
-    }
-
-    /**
-     * 查询本季度新增
-     * @return $this
-     */
-    public function quarterCreate()
-    {
-        return $this->andWhere('quarter(FROM_UNIXTIME(created_at)) = quarter(curdate())');
     }
 }
